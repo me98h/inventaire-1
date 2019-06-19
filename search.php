e0e2599be0cd9f326b28226e48897c102660958a
me98h <48553174+me98h@users.noreply.github.com>
2019-06-19 10:38:08 +0200

    Add files via upload

--- search.php
+++ search.php
@@ -1,22 +1,23 @@
 <?php  
- $connect = mysqli_connect("localhost", "root", "123456789", "inventaireucp");  
+ $connect = mysqli_connect("localhost", "root", "", "inventaireucp");  
  if(isset($_POST["query"]))  
  {  
       $output = '';  
-      $query = "SELECT nom,prenom FROM utilisateurs WHERE nom LIKE '%".$_POST["query"]."%' or prenom LIKE '%".$_POST["query"]."%'";  
-      $query2 = "SELECT nom_groupe FROM groupe WHERE nom_groupe LIKE '%".$_POST["query"]."%'"; 
+      $query = "SELECT no_emp_util,nom,prenom FROM utilisateurs WHERE nom LIKE '%".$_POST["query"]."%' or prenom LIKE '%".$_POST["query"]."%'";  
+      $query2 = "SELECT no_emp_grou,nom_groupe FROM groupe WHERE nom_groupe LIKE '%".$_POST["query"]."%'"; 
       $result = mysqli_query($connect, $query);  
       $result2 = mysqli_query($connect, $query2);  
+      $result = mysqli_query($connect, $query);  
       $output = '<ul class="list-unstyled">';  
       if(mysqli_num_rows($result) > 0 && mysqli_num_rows($result2) > 0)  
       {  
            while($row = mysqli_fetch_array($result))  
            {  
-                $output .= '<li class="country">'.$row["nom"].' '.$row["prenom"].'</li>';  
+                $output .= '<li class="country">'.$row["nom"].' '.$row["prenom"].' '.$row["no_emp_util"].'</li>';  
            }  
            while($row2 = mysqli_fetch_array($result2))  
            {  
-                $output .= '<li class="country">'.$row2["nom_groupe"].'</li>';  
+                $output .= '<li class="country">'.$row2["nom_groupe"].' '.$row2["no_emp_grou"].'</li>';  
            } 
       }  
       else  
